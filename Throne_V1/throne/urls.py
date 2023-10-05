fb428da54a1b8b82a091105d4caf130aef4f697a
75bluecodes <dennismuthusimutuku@gmail.com>
2023-10-05 11:58:10 +0300

    Frontend template

---
+++ Throne_V1/throne/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('about', views.about, name='about'),
+    path('userboard', views.userboard, name='userboard'),
+    path('feedback', views.feedback, name='feedback'),
+    path('settings', views.settings, name='settings'),
+    path('update/<str:email>', views.update, name='update'),
+    
+]
